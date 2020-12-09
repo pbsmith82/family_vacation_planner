ed18bd61bfca05d1f4eae0192cb4bdd8811c03a5
Pbsmith82 <pbsmith82@gmail.com>
2020-12-09 07:40:58 -0500

    Removed pwd from Validation - Could cause Errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
     belongs_to :room
     has_secure_password
 
-    validates_presence_of :first_name, :last_name, :email, :password
+    validates_presence_of :first_name, :last_name, :email
   end
